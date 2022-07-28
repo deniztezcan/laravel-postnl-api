22e5babee17db48c5484c19feab4604fddb4066d
Deniz Tezcan <d.tezcan42@gmail.com>
2022-07-28 13:37:58 +0200

    multilabel institution

--- src/Services/Converter.php
+++ src/Services/Converter.php
@@ -71,4 +71,80 @@ class Converter
 
         return $data;
     }
+
+    public static function MultiLabel(
+        Customer $customer,
+        LabellingMessage $message,
+        array $shipments
+    ) {
+        $data = new \stdClass();
+        $data->Customer = new \stdClass();
+        $data->Customer->Address = new \stdClass();
+        $data->Customer->Address->AddressType = $customer->getAddress()->getAddressType();
+        $data->Customer->Address->City = $customer->getAddress()->getCity();
+        $data->Customer->Address->CompanyName = $customer->getAddress()->getCompanyName();
+        $data->Customer->Address->Countrycode = $customer->getAddress()->getCountrycode();
+        $data->Customer->Address->HouseNr = $customer->getAddress()->getHouseNr();
+        $data->Customer->Address->HouseNrExt = $customer->getAddress()->getHouseNrExt();
+        $data->Customer->Address->Street = $customer->getAddress()->getStreet();
+        $data->Customer->Address->Zipcode = $customer->getAddress()->getZipcode();
+        $data->Customer->CollectionLocation = $customer->getCollectionLocation();
+        $data->Customer->CustomerCode = $customer->getCustomerCode();
+        $data->Customer->CustomerNumber = $customer->getCustomerNumber();
+        $data->Customer->Email = $customer->getEmail();
+
+        $data->Message = new \stdClass();
+        $data->Message->MessageID = 1;
+        $data->Message->MessageTimeStamp = Carbon::now()->format('d-m-Y H:i:s');
+        $data->Message->Printertype = $message->Printertype;
+
+        $shipmentsAddresses = [];
+        $shipmentsContacts = [];
+        $shipmentsArr = [];
+
+        foreach ($shipments as $shipment) {
+            if(count($shipmentsAddresses) == 0) {
+                foreach ($shipment->getAddresses() as $shipmentAddress) {
+                    $address = new \stdClass();
+                    $address->AddressType = $shipmentAddress->getAddressType();
+                    $address->City = $shipmentAddress->getCity();
+                    $address->CompanyName = $shipmentAddress->getCompanyName();
+                    $address->Countrycode = $shipmentAddress->getCountrycode();
+                    $address->HouseNr = $shipmentAddress->getHouseNr();
+                    $address->HouseNrExt = $shipmentAddress->getHouseNrExt();
+                    $address->Street = $shipmentAddress->getStreet();
+                    $address->Zipcode = $shipmentAddress->getZipcode();
+                    $shipmentsAddresses[] = $address;
+                }
+                foreach ($shipment->getContacts() as $shipmentContact) {
+                    $contact = new \stdClass();
+                    $contact->ContactType = $shipmentContact->getContactType();
+                    $contact->Email = $shipmentContact->getEmail();
+                    $contact->SMSNr = $shipmentContact->getSMSNr();
+                    $contact->TelNr = $shipmentContact->getTelNr();
+                    $shipmentsContacts[] = $contact;
+                }
+            }
+
+            $xshipment = new \stdClass();
+            $xshipment->Addresses = $shipmentsAddresses;
+            $xshipment->Barcode = $shipment->getBarcode();
+            $xshipment->Contacts = $shipmentsContacts;
+            $xshipment->DeliveryAddress = $shipment->getDeliveryAddress();
+            $xshipment->ProductCodeDelivery = $shipment->getProductCodeDelivery();
+            $xshipment->Reference = $shipment->getReference();
+            $xshipment->Remark = $shipment->getRemark();
+            $xshipment->Groups = new \stdClass();
+            $xshipment->Groups->Group = new \stdClass();
+            $xshipment->Groups->Group->GroupCount = $shipment->getGroups()->getGroupCount();
+            $xshipment->Groups->Group->GroupSequence = $shipment->getGroups()->getGroupSequence();
+            $xshipment->Groups->Group->GroupType = $shipment->getGroups()->getGroupType();
+            $xshipment->Groups->Group->MainBarcode = $shipment->getGroups()->getMainBarcode();
+            $shipmentsArr[] = $xshipment;
+        }
+
+        $data->Shipments = $shipmentsArr;
+
+        return $data;
+    }
 }
